29d25c7d9f4774ad0db2706bab51ce6affc878fc
fahrizal <muchamad.fachrizal12@gmail.com>
2025-01-11 01:56:30 +0700

    fix data

--- controllers/sensorController.js
+++ controllers/sensorController.js
@@ -5,22 +5,27 @@ const SensorData = require('../models/SensorData');
  */
 exports.handleSensorData = async (username, message) => {
   try {
+    // Parse message jika dalam bentuk string
+    const data = typeof message === 'string' ? JSON.parse(message) : message;
+    
     // Periksa apakah pengguna sudah ada di database
     const userSensorData = await SensorData.findOne({ where: { username } });
 
     if (userSensorData) {
-      // Jika sudah ada, perbarui kolom last_data
-      userSensorData.last_data = message;
+      // Jika sudah ada, perbarui kolom last_data dan data lainnya
+      userSensorData.last_data = data.data;
+      userSensorData.tegangan = data.tegangan;
+      userSensorData.waktu = data.waktu;
       await userSensorData.save();
-      console.log(`Updated last_data for user: ${username}`);
+      console.log(`Updated data for user: ${username}`);
     } else {
-      // Jika belum ada, buat entri baru dengan first_data
+      // Jika belum ada, buat entri baru
       await SensorData.create({
         username,
-        first_data: message,
-        last_data: message,
-        tegangan: 0,
-        waktu: new Date().toISOString(),
+        first_data: data.data,
+        last_data: data.data,
+        tegangan: data.tegangan,
+        waktu: data.waktu
       });
       console.log(`Created new entry for user: ${username}`);
     }
